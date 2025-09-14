c96a65b214a47cca09ffe9a29ea379c839c76220
Arsalan-khalili <arsalankhalili688@gmail.com>
2025-09-14 15:58:54 +0330

    Add HomePageView | CustomLoginView | base.html | template; fix template paths

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    
+class HomePageView(TemplateView):
+    template_name = 'users/home.html'
+
