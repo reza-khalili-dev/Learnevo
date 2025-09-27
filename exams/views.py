91a3f83ad5021bfeb47b23bdc719b34858a9e354
Arsalan-khalili <arsalankhalili688@gmail.com>
2025-09-27 19:35:30 +0330

    refactor(exams): convert exam views to class-based views
    - Added views with CBVs (TakeExamView, ExamResultView)
    - Added LoginRequiredMixin for access control
    - Organized exam flow: GET for displaying exam/questions, POST for saving answers and grading
    - Ensured consistency with other apps using class-based views

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from .models import Exam, Choice, StudentAnswer
+from .services import calculate_exam_score
+
 
 # Create your views here.
+
+class TakeExamView(LoginRequiredMixin, View):
+    def get(self, request, exam_id):
+        exam = get_object_or_404.filter(Exam, exam_id)
+        questions = exam.questions.all()
+        return render(request, "exams/take_exam.html", {"exam": exam, "questions": questions})
+
+    def post(self, request, exam_id):
+        exam = get_object_or_404(Exam, id=exam_id)
+        questions = exam.questions.all()
+        
+        for question in questions:
+            choice_id = request.POST.get(str(question.id))
+
+            if choice_id:
+                choice = get_object_or_404(Choice, id=choice_id, question=question)
+
+                StudentAnswer.objects.update_or_create(
+                    student=request.user,
+                    question=question,
+                    defaults={"choice": choice},
+                )
+        
+        result = calculate_exam_score(request.user, exam)
+        messages.success(request, f"Your exam is submitted. Score: {result.score}")
+        return redirect("exam_result", exam_id=exam.id)
+
+class ExamResultView(LoginRequiredMixin, View):
+    def get(self, request, exam_id):
+        exam = get_object_or_404(Exam, id=exam_id)
+        result = exam.results.filter(student=request.user).first()
+        return render(request, "exams/exam_result.html", {"exam": exam, "result": result})
