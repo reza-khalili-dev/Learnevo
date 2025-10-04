432bc328f1b924fee4df515b885429ac0969f1c2
Arsalan-khalili <arsalankhalili688@gmail.com>
2025-10-04 18:56:12 +0330

    feat(materials): restrict book management views to admins
    - BookListView & BookDetailView remain public (no restrictions)
    - Added LoginRequiredMixin + UserPassesTestMixin for BookCreate, BookUpdate, and BookDelete views
    - Only users with role manager or employee can add, edit, or delete books
    - Ensured consistency with role-based access control used in other apps

--- books/views.py
+++ books/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import  ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
+
 
 # Create your views here.
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'materials/book_list.html'
+    context_object_name = 'books'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'materials/book_detail.html'
+    context_object_name = 'book'
+
+
+class BookCreateView(LoginRequiredMixin,UserPassesTestMixin,CreateView):
+    model = Book
+    template_name = 'materials/book_form.html'
+    fields = ["title", "author", "price", "is_digital"]
+    success_url = reverse_lazy('book-list')
+
+    def test_func(self):
+        return self.request.user.role in ["manager", "employee"]
+    
+class BookUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Book
+    template_name = 'materials/book_form.html'
+    fields = ["title", "author", "price", "is_digital"]
+    success_url = reverse_lazy('book-list')
+
+    def test_func(self):
+        return self.request.user.role in ["manager", "employee"]
+
+class BookDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Book
+    template_name = 'materials/book_confirm_delete.html'
+    success_url = reverse_lazy('book-list')
+
+    def test_func(self):
+        return self.request.user.role in ["manager", "employee"]
+        
