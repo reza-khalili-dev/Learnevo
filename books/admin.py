fccad3cba002743942e3553f51016087ca520998
Arsalan-khalili <arsalankhalili688@gmail.com>
2025-10-05 19:37:40 +0330

    feat(books): register Book in admin and update CBVs with correct fields
    - Registered Book model in admin via @admin.register(Book)
    - Fixed admin fieldsets and list_display (removed invalid 'is_available')
    - Updated list_filter to use 'book_type' and 'shipping_available'
    - Improved readability with readonly_fields and proper grouping
    
    - Updated BookCreateView, BookUpdateView, and BookDeleteView:
      • Replaced invalid 'is_digital' with valid model fields
      • Added description, book_type, file, external_link, stock, shipping_available
      • Restricted create/update/delete access to roles: manager, employee
      • Fixed success_url with namespace (materials:book-list)

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "book_type", "price", "stock", "is_physical", "created_at")
+    search_fields = ("title", "author")
+    list_filter = ("book_type", "shipping_available", "created_at")
+    
+    fieldsets = (
+        ("Basic Information", {
+            "fields": ("title", "author", "description", "price")
+        }),
+        ("Book Details", {
+            "fields": ("book_type", "file", "external_link", "stock", "shipping_available")
+        }),
+        ("Uploader & Time", {
+            "fields": ("uploaded_by", "created_at", "updated_at")
+        }),
+    )
+
+    readonly_fields = ("created_at", "updated_at")
--- books/views.py
+++ books/views.py
@@ -7,41 +7,52 @@ from .models import Book
 # Create your views here.
 
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Book
+
+
 class BookListView(ListView):
     model = Book
     template_name = 'materials/book_list.html'
     context_object_name = 'books'
-
-
+    
 class BookDetailView(DetailView):
     model = Book
     template_name = 'materials/book_detail.html'
     context_object_name = 'book'
 
-
-class BookCreateView(LoginRequiredMixin,UserPassesTestMixin,CreateView):
+class BookCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Book
     template_name = 'materials/book_form.html'
-    fields = ["title", "author", "price", "is_digital"]
-    success_url = reverse_lazy('book-list')
-
-    def test_func(self):
-        return self.request.user.role in ["manager", "employee"]
-    
-class BookUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
-    model = Book
-    template_name = 'materials/book_form.html'
-    fields = ["title", "author", "price", "is_digital"]
-    success_url = reverse_lazy('book-list')
+    fields = [
+        "title", "author", "description", "book_type",
+        "price", "file", "external_link", "stock", "shipping_available"
+    ]
+    success_url = reverse_lazy('materials:book-list')
 
     def test_func(self):
         return self.request.user.role in ["manager", "employee"]
 
-class BookDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Book
+    template_name = 'materials/book_form.html'
+    fields = [
+        "title", "author", "description", "book_type",
+        "price", "file", "external_link", "stock", "shipping_available"
+    ]
+    success_url = reverse_lazy('materials:book-list')
+
+    def test_func(self):
+        return self.request.user.role in ["manager", "employee"]
+
+
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Book
     template_name = 'materials/book_confirm_delete.html'
-    success_url = reverse_lazy('book-list')
+    success_url = reverse_lazy('materials:book-list')
 
     def test_func(self):
         return self.request.user.role in ["manager", "employee"]
-        
